ddb1c4cc461c2c17dc9ab0fa03d017c883217063
Hardik Sisodiya <hardik04062003@gmail.com>
2024-05-29 16:00:01 +0530

    Repo initialize jay shree ram

---
+++ src/script.js
@@ -0,0 +1,25 @@
+// Importing the libraries
+import "remixicon/fonts/remixicon.css";
+import Lenis from "@studio-freight/lenis";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+// Scroll Trigger
+gsap.registerPlugin(ScrollTrigger);
+
+// Lenis js
+
+const lenisJs = () => {
+  const lenis = new Lenis();
+
+  lenis.on("scroll", (e) => {});
+
+  lenis.on("scroll", ScrollTrigger.update);
+
+  gsap.ticker.add((time) => {
+    lenis.raf(time * 500);
+  });
+
+  gsap.ticker.lagSmoothing(0);
+};
+lenisJs();
---
+++ src/three.js
@@ -0,0 +1,3 @@
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { gsap } from "gsap";
