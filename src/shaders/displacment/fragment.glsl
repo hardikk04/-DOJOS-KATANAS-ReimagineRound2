8a99a17da3e075d579bc7a58a1d46dd9fe31030e
Hardik Sisodiya <hardik04062003@gmail.com>
2024-06-18 02:37:16 +0530

    page1 animation almost done

--- src/shaders/displacment/fragment.glsl
+++ src/shaders/displacment/fragment.glsl
@@ -9,11 +9,11 @@ void main()
 {
     vec4 displacementTexture = texture2D(uDisplacmentTexture,vUv);
 
-    float displacementForce1 = displacementTexture.r * uOffset * 0.5;
+    float displacementForce1 = displacementTexture.r * uOffset * 1.0;
     vec2 uvDisplaced1 = vec2(vUv.x + displacementForce1, vUv.y + displacementForce1);
     vec4 displacementTexture1 = texture2D(uTexture1,uvDisplaced1);
 
-    float displacementForce2 = displacementTexture.r * (1.0 - uOffset) * 1.0;
+    float displacementForce2 = displacementTexture.r * (1.0 - uOffset) * 2.0;
     vec2 uvDisplaced2 = vec2(vUv.x - displacementForce2, vUv.y - displacementForce2);
     vec4 displacementTexture2 = texture2D(uTexture2,uvDisplaced2);
 
