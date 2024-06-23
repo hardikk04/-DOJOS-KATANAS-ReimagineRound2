8ceb03b4e9347a1dcf176078169a4e0d27d753a5
Ayush Ahirwar <ayushahirwar6673@gmail.com>
2024-06-23 16:11:23 +0530

    main conflict resolved

--- src/script.js
+++ src/script.js
@@ -26,47 +26,13 @@ const lenisJs = () => {
 lenisJs();
 
 
+
+
+
+
 // page7Animation
-const page7Animation = () => {
+const page7Animation = ()=>{
   var tl7 = gsap.timeline({
-
-    scrollTrigger: {
-      trigger: "#page7",
-      scroller: "body",
-      start: "25% 40%",
-      end: "25% -20%",
-      scrub: 1,
-      // markers:true
-    },
-  });
-
-  tl7.to("#discovery #dis-text", {
-    scale: 1,
-    clipPath: "polygon(0% 0%, 100% 0, 100% 100%, 0% 100%)",
-    duration: 2,
-    ease: "linear",
-  });
-
-  var tl72 = gsap
-    .timeline({
-      scrollTrigger: {
-        trigger: "#page7",
-        scroller: "body",
-        start: "bottom bottom",
-        end: "bottom -50%",
-        pin: true,
-        scrub: 2,
-        // markers:true
-      },
-    })
-    .to("#discovery #dis-text h1", {
-      x: "-72%",
-      duration: 3,
-      ease: "linear",
-    });
-};
-page7Animation();
-
     scrollTrigger:{
       trigger:"#discovery",
       scroller:"body",
@@ -180,4 +146,3 @@ const textEffect = () => {
   });
 };
 textEffect();
-
